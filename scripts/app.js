7231cb85f39b10fa523e7e40cabe2b688c8f6285
Andrew Bean <andrew.bean06@imperial.ac.uk>
2016-07-21 20:03:11 +0100

    Fixed OSM map data to be served over HTTPS

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 var map = L.map('map');
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
     maxZoom: 18,
 	minZoom: 8,
